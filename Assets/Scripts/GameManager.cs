94abad3818a8d7cbd4287760841582fb7560eff8
unknown <alvaro500@outlook.com>
2022-09-13 09:22:42 -0500

    1) Base del GameManager lista

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action onMainMenu;
+    public event Action onItemsMenu;
+    public event Action onARPosition;
+
+    public static GameManager instance;
+    
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        onMainMenu?.Invoke();//Constatar que existe que está suscrito a este evento.
+        Debug.Log("Main Menu Activated");
+    }
+
+    public void ItemsMenu()
+    {
+        onItemsMenu?.Invoke();//Constatar que existe que está suscrito a este evento.
+        Debug.Log("Items Menu Activated");
+    }
+
+    public void ARPosition()
+    {
+        onARPosition.Invoke();
+        Debug.Log("AR position Activated");
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
